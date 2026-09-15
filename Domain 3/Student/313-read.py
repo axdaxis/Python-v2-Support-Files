0d4a4e5d586ba6823bed3dccbd2aac5d8effb1e2
Dax <38143460+axdaxis@users.noreply.github.com>
2026-09-15 10:13:50 -0500

    updates

--- Domain 3/Student/313-read.py
+++ Domain 3/Student/313-read.py
@@ -4,6 +4,6 @@ message.write('Testing file for player score')
 message.close()
 
 message_test = open('313-message.txt','r')
-
-
+msg = message_test.read()
+print(msg)
 message_test.close()
---
+++ README.md
@@ -0,0 +1,3 @@
+# this is for a class
+
+i know this is on my public GitHub just ignore it 
